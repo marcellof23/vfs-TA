53c4f4c49d248f981740d753fe8a8347f0e9144e
Marcello Faria <01faria.marcello@gmail.com>
2023-02-18 22:29:00 +0700

    feat: init vfs service

---
+++ cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var file string
+
+var rootCmd = &cobra.Command{
+	Use:   "vfs",
+	Short: "An Intermediate application",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func Execute() error {
+	rootCmd.PersistentFlags().StringVarP(&file, "config", "c", "config.yaml", "Config file")
+
+	// sub commands are added in respective files
+	return rootCmd.Execute()
+}
---
+++ cmd/vfs/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/marcellof23/vfs-TA/cmd"
+)
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+
+		os.Exit(1)
+	}
+
+}
